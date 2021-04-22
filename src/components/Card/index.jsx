295419d87c7022e6e22a0fb96343da01ddb5f8a1
Surin Viktor <vsurin.upitel@gmail.com>
2021-04-22 09:48:58 +0300

    #7: Сделал подсвечивание активного репозитория при нажатии на tab

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,10 +3,18 @@ import "./Card.css";
 export default (props) => {
     const { avatar_url, id, login, html_url } = props.data;
 
+    const handleOnFocus = (e) => {
+        let elements = document.querySelectorAll(".card");
+        [].forEach.call(elements, function(el) {
+            el.classList.remove("active");
+        });
+        e.target.parentNode.classList.add("active");
+    };
+
     return (
       <div className="card" id={id}>
           <img src={avatar_url} alt="User Avatar"/>
-          <a target="_blank" href={html_url}><h4>Link: {login}</h4></a>
+          <a target="_blank" href={html_url} onFocus={handleOnFocus}><h4>Link: {login}</h4></a>
       </div>
     );
 }
